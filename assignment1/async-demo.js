e280e2ad1c6040a0ddf81ef62fa2d2349c7a818b
Adrian <31854027+pleaseHelpImDumb@users.noreply.github.com>
2025-11-14 18:27:27 -0500

    Assignment 1

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -1,16 +1,117 @@
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+const fsp = require("fs/promises");
 
 // Write a sample file for demonstration
 
 // 1. Callback style
+// Callback hell example (test and leave it in comments):
+// 2. Promise style
+// 3. Async/Await style
 
+const writeSample = async () => {
+  fs.writeFile(
+    __dirname + "/sample-files/sample.txt",
+    "Hello, async world!",
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
 
-  // Callback hell example (test and leave it in comments):
+//Basic read file
+const readFile = async () => {
+  fs.readFile(__dirname + "/sample-files/sample.txt", "utf8", (err, data) => {
+    if (err) {
+      console.log("Error: ", err);
+    } else {
+      console.log("Callback read:", data);
+    }
+  });
+};
 
+/*
+The previous function is a callback function with (err, data)=>{}
+If readFile has an error, it is logged on line 14
+If successful, the 'data' (contents of file) is logged
 
-  // 2. Promise style
+Callback hell would occur trying to write to a file.
+For example, writing two lines to a file would look like this:
 
+fs.open("./file", ..., (err, fileHandle) => {
+  if(err) {console.log(err);}
+  else{
+      COOL! We have the handle. Now we can do stuff with it.
+      But now writing requires another callback:
+      
+      fd.write(fd, 'some text!', (err) => {
+        if(err){console.log('another error...');}
+        
+        COOL! We wrote the line! Now close the file...?
+        fd.close(fd, (err) =>{
+          ...
+        });
+      });
+  }
+});
+*/
 
-      // 3. Async/Await style
+//Writing to 'writing.txt' with promises:
+/*
+const writeFile = async (fileName, text) => {
+  try {
+    const fh = await new Promise((resolve, reject) => {
+      fs.open(fileName, "w", (err, fh) => {
+        return err ? reject(err) : resolve(fh);
+      });
+    });
+
+    await new Promise((resolve, reject) => {
+      fs.write(fh, text, (err) => {
+        return err ? reject(err) : resolve();
+      });
+    });
+
+    await new Promise((resolve, reject) => {
+      fs.close(fh, (err) => {
+        return err ? reject(err) : resolve();
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+*/
+//Reading with promises:
+const readFilePromise = async (filePath) => {
+  try {
+    const data = await new Promise((resolve, reject) => {
+      fs.readFile(filePath, "utf8", (err, data) => {
+        return err ? reject(err) : resolve(data);
+      });
+    });
+    console.log("Promise read: ", data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//Reading with async/await
+const readFileAwait = async (filePath) => {
+  try {
+    const data = await fsp.readFile(filePath, "utf8");
+    console.log("Async/Await read: ", data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const consoleLog = async () => {
+  await writeSample();
+  await readFile();
+  await readFilePromise(__dirname + "/sample-files/sample.txt");
+  await readFileAwait(__dirname + "/sample-files/sample.txt");
+};
+
+consoleLog();
--- assignment1/core-modules-demo.js
+++ assignment1/core-modules-demo.js
@@ -1,18 +1,76 @@
-const os = require('os');
-const path = require('path');
-const fs = require('fs');
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const fsp = require("fs/promises"); //
 
-const sampleFilesDir = path.join(__dirname, 'sample-files');
+const sampleFilesDir = path.join(__dirname, "sample-files");
 if (!fs.existsSync(sampleFilesDir)) {
   fs.mkdirSync(sampleFilesDir, { recursive: true });
 }
 
 // OS module
-
-
+console.log("Platform: ", os.platform());
+console.log("CPU:", os.cpus()[0].model, "@", os.cpus()[0].speed / 1000, "GHz");
+console.log("Total Memory:", os.totalmem());
 // Path module
+const joinedPath = path.join("/path/to", "sample-files", "folder/file.txt");
+console.log("Joined path: ", joinedPath);
 
 // fs.promises API
-
+const fspWrite = async () => {
+  try {
+    await fsp.writeFile(
+      __dirname + "/sample-files/demo.txt",
+      "Hello from fs.promises!"
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+const fspRead = async () => {
+  try {
+    let data = await fsp.readFile(__dirname + "/sample-files/demo.txt", "utf8");
+    console.log("fs.promises read:", data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // Streams for large files- log first 40 chars of each chunk
+const fspWriteLarge = async () => {
+  try {
+    let text =
+      "The fs/promises API provides asynchronous file system methods that return promises.\nThe promise APIs use the underlying Node.js threadpool to perform file system operations off the event loop thread.\nThese operations are not synchronized or threadsafe. Care must be taken when performing multiple concurrent modifications on the same file or data corruption may occur.\n";
+
+    await fsp.writeFile(__dirname + "/sample-files/largefile.txt", "");
+    for (let i = 1; i < 100; i++) {
+      await fsp.appendFile(__dirname + "/sample-files/largefile.txt", text);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const fspReadStream = () => {
+  const rs = fs.createReadStream(__dirname + "/sample-files/largefile.txt", {
+    encoding: "utf8",
+    highWaterMark: 1024,
+  });
+
+  rs.on("data", (chunk) => {
+    console.log("Read chunk: ", chunk.substring(0, 40));
+  });
+
+  rs.on("end", () => {
+    console.log("Finished reading large file with streams.");
+  });
+};
+
+// for printing in order
+const consoleLog = async () => {
+  await fspWrite();
+  await fspRead();
+  await fspWriteLarge();
+  await fspReadStream();
+};
+consoleLog();
