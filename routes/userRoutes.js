71b49424ad083cdf3054b2a3add7035f2d1d2bc1
Adrian <31854027+pleaseHelpImDumb@users.noreply.github.com>
2026-01-22 14:44:05 -0500

    jwtmiddleware before logOff

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwtMiddleware = require("../middleware/jwtMiddleware.js");
 
 const router = express.Router();
 const { register, logon, logoff } = require("../controllers/userController");
@@ -8,6 +9,7 @@ router.route("/register").post(register);
 
 router.route("/logon").post(logon);
 
+router.use(jwtMiddleware);
 router.route("/logoff").post(logoff);
 
 module.exports = router;
